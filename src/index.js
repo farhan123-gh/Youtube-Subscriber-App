bfc5b2c429c0e3415878487fd2211c95ee94e8c3
Farhan Ahamad <farhan000ahamad@gmail.com>
2024-11-02 12:07:00 +0530

    index.js correction

--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ const app = require('./app'); // Import the Express app
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/subscribersDB';
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(MONGODB_URI)
+.then(() => {
   console.log('Connected to MongoDB');
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
