1b629a0ff2239d6c11255ea7762f1eba413fb9d1
Farhan Ahamad <farhan000ahamad@gmail.com>
2024-11-04 21:21:34 +0530

    update gitingore and index.js

--- src/index.js
+++ src/index.js
@@ -9,10 +9,11 @@ const MONGODB_URI = process.env.MONGODB_URI; // Remote URI from .env file
 mongoose.connect(MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
+    app.listen(PORT, '0.0.0.0', () => {  // Change 'localhost' to '0.0.0.0'
+      console.log(`Server running on http://0.0.0.0:${PORT}`);
     });
   })
   .catch(error => {
     console.error('Database connection error:', error);
   });
+
